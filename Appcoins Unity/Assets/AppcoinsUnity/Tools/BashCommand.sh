e8c5e3c4dc507a8d03aec4064e9244b44fdc35ec
Fábio Vital <fabio.vital@aptoide.com>
2018-09-19 17:57:15 +0100

    new dll files

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
 cd '/Users/aptoide/Desktop/build/Appcoins Unity'
-'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' assembleRelease 2>&1 2>'/Users/aptoide/Documents/GitHub/asf-unity-plugin/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' assembleDebug 2>&1 2>'/Users/aptoide/Documents/GitHub/asf-unity-plugin/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 echo 'done' > '/Users/aptoide/Documents/GitHub/asf-unity-plugin/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
